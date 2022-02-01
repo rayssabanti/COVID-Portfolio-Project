7899a9464c950ae02c51e44faeda93e36e3683bc
Rayssa Banti <rayssabanti@hotmail.com>
2022-02-01 14:02:59 -0300

    Análise Exploratória

--- Análise Exploratória.sql
+++ Análise Exploratória.sql
@@ -32,20 +32,22 @@ order by 1,2
 
 -- Total de Casos x População
 -- Mostra a porcentagem da população que contraiu covid
-Select location as [Localização], date as [Data], total_cases as [Contagem de Casos], population as [População], (total_cases/population) * 100 as [Porcentagem da população com Covid]
+Select location as [Localização], date as [Data], max(total_cases) as [Contagem de Casos], max(population) as [População], max((total_cases/nullif(population,0))) * 100 as [Porcentagem da população com Covid]
 from [PortfolioProject].[dbo].[covid-deaths]
-where location like '%Brazil%' and  continent <> ''
-order by 1,2 
+where  continent <> ''
+group by Location, Population, date
+order by [Porcentagem da população com Covid] desc
+ 
 
 
 -- Países com a maior taxa de infecção comparado com a População 
-Select location as [Localização], population [População],  MAX(total_cases) as [Contagem de Casos], Max((total_cases/nullif(population,0))) * 100 as [Porcentagem da população com Covid]
+Select location as [Localização], population [População],  MAX(total_cases) as [Contagem de Casos], Max((total_cases * 100/nullif(population,0)))  as [Porcentagem da população com Covid]
 from [PortfolioProject].[dbo].[covid-deaths]
 where  continent <> ''
 group by Location, Population
 order by  [Porcentagem da população com Covid] desc
 
--- Mostrando os países com a maior taxa de morte por população 
+-- Mostrando os países com a maior contagem de mortes por população 
 Select location as [Localização], MAX(Total_deaths) as [Contagem de Mortes]
 from [PortfolioProject].[dbo].[covid-deaths]
 where  continent <> ''
@@ -60,8 +62,11 @@ where  continent = '' and location in ('Europe', 'Asia', 'North America', 'South
 group by  location
 order by  [Contagem de Mortes] desc
 
+
+
 -- Olhando globalmente 
 Select SUM(new_cases) as [Contagem de Casos] , sum(new_deaths) as [Contagem de Mortes]
+,  SUM(cast(new_deaths as int)*100)/SUM(New_Cases) as [Porcentagem de mortes]
 from [PortfolioProject].[dbo].[covid-deaths]
 where  continent <> ''
 
@@ -69,7 +74,8 @@ where  continent <> ''
 
 
 -- Olhando a quantidade de pessoas vacinadas por país
-select CD.location, MAX(cv.people_vaccinated) as [Contagem de pessoas Vacinadas], cd.population as [População], MAX(cv.people_vaccinated) / nullif(population,0) * 100 as [Porcentagem de pessoas vacinadas]
+select CD.location, MAX(cv.people_vaccinated) as [Contagem de pessoas Vacinadas], cd.population as [População], 
+MAX(cv.people_vaccinated) / nullif(population,0) * 100 as [Porcentagem de pessoas vacinadas]
 from [PortfolioProject].[dbo].[covid-deaths] CD
 join [dbo].[covid-vaccinations] CV
 	on cd.location = cv.location
@@ -81,19 +87,21 @@ order by 1,2
 
 -- Olhando a quantidade de pessoas vacinadas por dia 
 
-select cd.continent, cd.location, cd.date, cd.population, cv.new_vaccinations
-, sum(cv.new_vaccinations) over (Partition by cd.location order by cd.location, cd.date) as PessoasVacinadas 
-into [PortfolioProject].[dbo].[Population_Vaccinations]
+select  cd.location as [Localização], cd.date as [Data], cd.population as [População], cv.new_vaccinations as [Novas Vacinações]
+, sum(cv.new_vaccinations) over (Partition by cd.location order by cd.location, cd.date) as [Pessoas Vacinadas], cd.new_deaths as [Novas Mortes]
+--into [PortfolioProject].[dbo].[populacao_vacinada]
 from [PortfolioProject].[dbo].[covid-deaths] CD
 join [PortfolioProject].[dbo].[covid-vaccinations] CV
 	on cd.location = cv.location
 	and cd.date = cv.date
 where  CD.continent <> '' 
+order by 1,2
 
-
-select pv.*, (PessoasVacinadas/nullif(population,0) * 100) as TotalPessoasVacinadas
-from [PortfolioProject].[dbo].[Population_Vaccinations] PV
+select pv.*, ([Pessoas Vacinadas]/nullif( [População],0) )* 100 as [Taxa de Pessoas Vacinadas]
+from [PortfolioProject].[dbo].[populacao_vacinada] PV
 order by 2,3 
+
+
 
 -- Criando View para ajudar depois 
 CREATE VIEW 
